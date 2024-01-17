2709301737530a3af51383d595cda5e2de201d0c
Xal0s <kevinpereira95@free.fr>
2024-01-17 11:51:16 +0100

    integration de la banniere sur l'index

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="index.css">
+    <title>Mov'Ease - Accueil</title>
+</head>
+<body>
+    <img src="images/banniereMovEase.jpg" class ="banniere" alt="banniere du site Mov'Ease">
+    <br>
+    <br>
+    <div class="categories">
+        <div class="newProducts">
+            <h2>Nouveautés</h2>
+            <div class ="horizontalScroll">
+                <?php 
+                ?>
+            </div>
+        </div>
+        <div class="promotions">
+            <h2>Promotions</h2>
+            <div class ="horizontalScroll">
+                <?php 
+                ?>
+            </div>
+        </div>
+        <div class="actionType">
+            <h2>Action</h2>
+            <div class ="horizontalScroll">
+                <?php 
+                ?>
+            </div>
+        </div>
+        <div class="kidsType">
+            <h2>Enfants</h2>
+            <div class ="horizontalScroll">
+                <?php 
+                ?>
+            </div>
+        </div>
+        <div class="fantasticType">
+            <h2>Fantastique</h2>
+            <div class ="horizontalScroll">
+                <?php 
+                ?>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+</body>
+</html>
