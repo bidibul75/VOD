598b743508671881a39aec6bdc87e54153b04ad0
WillyB89 <walid.ben-aissa@edu.esiee-it.fr>
2024-01-19 16:59:06 +0100

    page login + page creation nouveau compte + page reset password

---
+++ registerUser.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription - MovEase</title>
+    <link rel="stylesheet" href="CSS/header-footer.css">
+</head>
+<body>
+
+    
+        <?php include("header.html");?>
+    
+
+    <main>
+        <div class="login-container">
+            <h2>Inscription à MovEase</h2>
+            <form action="process_registration.php" method="post">
+                <label for="username">Nom d'utilisateur :</label>
+                <input type="text" id="username" name="username" required>
+
+                <label for="email">Adresse e-mail :</label>
+                <input type="email" id="email" name="email" required>
+
+                <label for="password">Mot de passe :</label>
+                <input type="password" id="password" name="password" required>
+
+                <label for="confirm_password">Confirmer le mot de passe :</label>
+                <input type="password" id="confirm_password" name="confirm_password" required>
+
+                <button type="submit">S'inscrire</button>
+            </form>
+        </div>
+    </main>
+
+    
+        <?php include("footer.html");?>
+    
+
+</body>
+</html>
