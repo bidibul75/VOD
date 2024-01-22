766f78950d157d3d5fed56ae4bd699d951b05b71
bidibul75 <walter.bianchi@edu.esiee-it.fr>
2024-01-22 11:40:39 +0100

    Revert "Modification index.php : include du fichier header.php."
    This reverts commit 86237565b6723bf71236bd095eb0148357ae50fb.

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
     <title>Mov'Ease - Accueil</title>
 </head>
 <body>
-    <?php include("header.php");?>
+    <?php include("header.html");?>
     <img src="logos/banniereMovEase.jpg" class ="banniere" alt="banniere du site Mov'Ease">
     <br>
     <br>
