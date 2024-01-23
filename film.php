919e08b4ac3648b8eea3902f984728aec7025d8b
bidibul75 <walter.bianchi@edu.esiee-it.fr>
2024-01-23 14:17:45 +0100

    Rebase panier / inProd.

--- film.php
+++ film.php
@@ -40,8 +40,11 @@ if (isset($_GET["id"])) {
         <div class="photoFilm">
             <img src="<?= $movie["imageProduit"] ?>" alt="affiche film" class="filmPhoto">
         </div>
-        <div class="synopsis">
-            <p><?= $movie["synopsis"] ?></p>
+        <div class = "synopsis">
+            <p>Synopsis : <br><?= $movie["synopsis"]?></p>
+        </div>
+        <div class="realisateur">
+           <p>Réalisateur : </p><h4><?= $movie["realisateur"]?></h4>
         </div>
         <div class="realisateur">
            <p>Réalisateur : </p><h4><?= $movie["realisateur"]?></h4>
